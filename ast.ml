f8de076eba1e412ab3d087734ec021729dd57fc0
1jiaqi3 <jiaqi.liu@vanderbilt.edu>
2018-02-16 21:03:15 -0500

    Changed type order

--- ast.ml
+++ ast.ml
@@ -3,7 +3,7 @@ type builtin = Add | Sub | Mult | Div | Mod | Eq | Neq | Lt | Gt | Leq | Geq |
 
 type basic = Int | Double | Bool | String | List of typ
 and lambda = typ list * typ
-and member = MemConst of string * typ * expr | MemVar of string * typ
+and member = MemConst of string * typ * expr | MemVar of typ * string
 and cls = string * member list * string list
 and typ = Basic of basic | Lambda of lambda | Class of cls | Void
 and defvar = typ * string * expr
