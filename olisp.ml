510b098c0b61a53777756534db6649d261913d99
Linli <lw2780@columbia.edu>
2018-02-20 13:37:29 -0500

    a

--- olisp.ml
+++ olisp.ml
@@ -1,8 +1,3 @@
-let () = 
- let usage_msg = "usage: ./olisp.native [-a|-s|-l|-c] [file.ol]" in
-  let channel = ref stdin in
-  Arg.parse [] (fun filename -> channel := open_in filename) usage_msg;
-   
 let string_of_typ = function
 	Int -> "int"
   | Bool -> "bool"
@@ -11,21 +6,22 @@ let string_of_typ = function
   | List -> "list"
   | Class -> "Class"
 
-let string_of_vdecl (t, id) = string_of_typ t ^ " " ^ id ";\n"
-
-let string_of_fdecl fdecl =
+let () = 
+ let usage_msg = "usage: ./olisp.native [-a|-s|-l|-c] [file.mc]" in
+  let channel = ref stdin in
+  Arg.parse [] (fun filename -> channel := open_in filename) usage_msg;
+  
+  let lexbuf = Lexing.from_channel !channel in
+  let ast = Parser.program Scanner.token lexbuf in  
+  let string_of_vdecl (t, id) = string_of_typ t ^ " " ^ id ";\n" in 
+  let string_of_fdecl fdecl =
   string_of_typ fdecl.typ ^ " " ^
   fdecl.fname ^ "(" ^ String.concat ", " (List.map snd fdecl.formals) ^
   ")\n{\n" ^
   String.concat "" (List.map string_of_vdecl fdecl.locals) ^
   String.concat "" (List.map string_of_stmt fdecl.body) ^
-  "}\n"
-
-let string_of_program (vars, funcs) =
+  "}\n" in
+  let string_of_program (vars, funcs) =
   String.concat "" (List.map string_of_vdecl vars) ^ "\n" ^
-  String.concat "\n" (List.map string_of_fdecl funcs)
-
-
-  let lexbuf = Lexing.from_channel !channel in
-  let ast = Parser.program Scanner.token lexbuf in  
+  String.concat "\n" (List.map string_of_fdecl funcs) in
   print_string (Ast.string_of_program ast)
