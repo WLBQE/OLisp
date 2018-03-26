0a447c05c19a231b99c88dcdd2fea1a298cbbb15
Libo Wang <lw2768@columbia.edu>
2018-03-26 04:37:21 -0400

    test

--- codegen.ml
+++ codegen.ml
@@ -8,13 +8,14 @@ let translate stoplevels =
   let context  = L.global_context () in
   let i32_t = L.i32_type context
   and i8_t = L.i8_type context
+  and void_t = L.void_type context
   and the_module = L.create_module context "OLisp" in
 
   let printf_t : L.lltype = L.var_arg_function_type i32_t [| L.pointer_type i8_t |] in
   let printf_func : L.llvalue = L.declare_function "printf" printf_t the_module in
 
-  let build_function fdecl =
-    let main_ty = L.function_type i32_t [||] in
+  let build_toplevel top =
+    let main_ty = L.function_type void_t [||] in
     let the_function = L.define_function "main" main_ty the_module in
     let builder = L.builder_at_end context (L.entry_block the_function) in
     let int_format_str = L.build_global_stringptr "%d\n" "i_fmt" builder in
@@ -23,8 +24,9 @@ let translate stoplevels =
       | SCall((BuiltIn, SBuiltIn(Print)), [exp]) -> L.build_call printf_func [| int_format_str ; (expr builder exp) |] "printf" builder
       | _ -> raise (Failure "To be implemented")
     in
-    let rec stmt builder = function
-        SExpr e -> let _ = L.build_ret (expr builder e) builder in builder
+    let rec toplevel builder = function
+        SExpr e -> ignore(expr builder e); L.build_ret_void builder
       | s -> raise (Failure "To be implemented")
-    in ignore(stmt builder fdecl)
-  in List.iter build_function stoplevels; the_module
+    in ignore(toplevel builder top)
+  in List.iter build_toplevel stoplevels;
+  the_module
--- test_hello.sh
+++ test_hello.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
-failed=0
+LLI="lli"
 
-./olisp.native -s tests/hello_world.olisp
+./olisp.native tests/hello_world.olisp > tests/hello_world.ll
 
-if [[ $? -eq 0 ]]; then
-	printf "Test passed!\n"
-else
-	printf "Test failed!\n"
-	((failed++))
+if [[ $? -ne 0 ]]; then
+	rm -f tests/hello_world.ll
+	printf "\nTest failed!\n"
+	exit 1
 fi
 
-exit $((failed))
+"$LLI" tests/hello_world.ll > tests/hello_world.result
+
+if [[ $? -ne 0 ]]; then
+	rm -f tests/hello_world.result tests/hello_world.ll
+	printf "\nTest failed!\n"
+	exit 1
+fi
+
+diff tests/hello_world.result tests/hello_world.out
+
+if [[ $? -ne 0 ]]; then
+	rm -f tests/hello_world.result tests/hello_world.ll
+	printf "\nTest failed!\n"
+	exit 1
+fi
+
+rm -f tests/hello_world.result tests/hello_world.ll
+printf "Test passed!\n"
+exit 0
