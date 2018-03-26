404bc9ab4e1ddef2637a6c9cba914692859b8cf4
Libo Wang <lw2768@columbia.edu>
2018-03-26 05:17:17 -0400

    update test script to use llc

--- test_hello.sh
+++ test_hello.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-LLI="lli"
+CC="cc"
+LLC="llc"
 
 ./olisp.native tests/hello_world.olisp > tests/hello_world.ll
 
@@ -10,22 +11,30 @@ if [[ $? -ne 0 ]]; then
 	exit 1
 fi
 
-"$LLI" tests/hello_world.ll > tests/hello_world.result
-
+"$LLC" tests/hello_world.ll > tests/hello_world.s
 if [[ $? -ne 0 ]]; then
-	rm -f tests/hello_world.result tests/hello_world.ll
+	rm -f tests/hello_world.ll tests/hello_world.s
 	printf "\nTest failed!\n"
 	exit 1
 fi
 
+"$CC" -o tests/hello_world.exe tests/hello_world.s
+
+if [[ $? -ne 0 ]]; then
+	rm -f tests/hello_world.ll tests/hello_world.s tests/hello_world.exe
+	printf "\nTest failed!\n"
+	exit 1
+fi
+
+tests/hello_world.exe > tests/hello_world.result
 diff tests/hello_world.result tests/hello_world.out
 
 if [[ $? -ne 0 ]]; then
-	rm -f tests/hello_world.result tests/hello_world.ll
+	rm -f tests/hello_world.ll tests/hello_world.s tests/hello_world.exe tests/hello_world.result
 	printf "\nTest failed!\n"
 	exit 1
 fi
 
-rm -f tests/hello_world.result tests/hello_world.ll
+rm -f tests/hello_world.ll tests/hello_world.s tests/hello_world.exe tests/hello_world.result
 printf "Test passed!\n"
 exit 0
