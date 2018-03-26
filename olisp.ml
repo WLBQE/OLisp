9ea874f2e30a9be7d42136f075b70f210c363772
Libo Wang <lw2768@columbia.edu>
2018-03-26 02:41:48 -0400

    啊啊啊啊啊

--- olisp.ml
+++ olisp.ml
@@ -17,6 +17,6 @@ let () =
   let ast = Parser.program Scanner.token lexbuf in
   match !action with
     Ast -> print_string (Ast.string_of_program ast)
-  | Sast    -> print_string (Sast.string_of_sprogram (Semant.check ast))
+  | Sast -> print_string (Sast.string_of_sprogram (Semant.check ast))
   | LLVM_IR -> print_string (Llvm.string_of_llmodule (Codegen.translate (Semant.check ast)))
   | Compile -> let m = Codegen.translate sast in Llvm_analysis.assert_valid_module m; print_string (Llvm.string_of_llmodule m)
