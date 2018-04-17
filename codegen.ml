25ec33960d94743599a0be97899b31624893061d
romuloc8 <romulobritannia@gmail.com>
2018-04-17 16:38:58 -0400

    Add comparison

--- codegen.ml
+++ codegen.ml
@@ -21,8 +21,49 @@ let translate (sym, cls, stoplevels) =
     let int_format_str = L.build_global_stringptr "%d\n" "i_fmt" builder in
     let rec expr builder ((_, e) : sexpr) = match e with
         SLit i -> L.const_int i32_t i
+      | SDoubleLit l -> L.const_float_of_string float_t l
+      | SBoolLit b -> L.const_int i1_t (if b then 1 else 0)
+      (*| SStringLit -> 
+      | SId s -> L.build_load (lookup s) s builder 
+      | SMemId*)
       | SCall ((SBuiltInTyp Print, SBuiltIn Print), [exp]) ->
         L.build_call printf_func [| int_format_str ; (expr builder exp) |] "printf" builder
+      (*| SCall ((SBuiltInTyp Add, SBuiltIn Add), [exp]) ->
+
+      | SCall ((SBuiltInTyp Sub, SBuiltIn Sub), [exp]) ->
+
+      | SCall ((SBuiltInTyp Mult, SBuiltIn Mult), [exp]) ->
+
+      | SCall ((SBuiltInTyp Div, SBuiltIn Div), [exp]) ->
+
+      | SCall ((SBuiltInTyp Mod, SBuiltIn Mod), [exp]) ->*)
+      | SCall ((SBuiltInTyp EQ, SBuiltIn EQ), e1, e2) ->
+        let (t, _) = e1 and e1' = expr builder e1 and e2' = expr builder e2 in
+        if t = A.Double then L.build_fcmp L.Fcmp.Oeq e1' e2' "tmp" builder
+        else L.build_icmp L.Icmp.Eq e1' e2' "tmp" builder
+      | SCall ((SBuiltInTyp Neq, SBuiltIn Neq), e1, e2) ->
+        let (t, _) = e1 and e1' = expr builder e1 and e2' = expr builder e2 in
+        if t = A.Double then L.build_fcmp L.Fcmp.One e1' e2' "tmp" builder
+        else L.build_icmp L.Icmp.Ne e1' e2' "tmp" builder
+      | SCall ((SBuiltInTyp Lt, SBuiltIn Lt), e1, e2) ->
+        let (t, _) = e1 and e1' = expr builder e1 and e2' = expr builder e2 in
+        if t = A.Double then L.build_fcmp L.Fcmp.Olt e1' e2' "tmp" builder
+        else L.build_icmp L.Icmp.Slt e1' e2' "tmp" builder 
+      | SCall ((SBuiltInTyp Gt, SBuiltIn Gt), e1, e2) ->
+        let (t, _) = e1 and e1' = expr builder e1 and e2' = expr builder e2 in
+        if t = A.Double then L.build_fcmp L.Fcmp.Ogt e1' e2' "tmp" builder
+        else L.build_icmp L.Icmp.Sgt e1' e2' "tmp" builder
+      | SCall ((SBuiltInTyp Leq, SBuiltIn Leq), e1, e2) ->
+        let (t, _) = e1 and e1' = expr builder e1 and e2' = expr builder e2 in
+        if t = A.Double then L.build_fcmp L.Fcmp.Ole e1' e2' "tmp" builder
+        else L.build_icmp L.Icmp.Sle e1' e2' "tmp" builder
+      | SCall ((SBuiltInTyp Geq, SBuiltIn Geq), e1, e2) ->
+        let (t, _) = e1 and e1' = expr builder e1 and e2' = expr builder e2 in
+        if t = A.Double then L.build_fcmp L.Fcmp.Oge e1' e2' "tmp" builder
+        else L.build_icmp L.Icmp.Sge e1' e2' "tmp" builder
+      (*| SCall (f, args) -> let (fdef, fdecl) = StringMap.find f 
+      | SLst
+      | SLambdaExpr *)
       | _ -> raise (Failure "To be implemented")
     in
     let rec toplevel builder = function
@@ -31,3 +72,4 @@ let translate (sym, cls, stoplevels) =
     in ignore(toplevel builder top)
   in List.iter build_toplevel stoplevels;
   the_module
+    
