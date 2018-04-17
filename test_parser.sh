6394f6b29efd40cfd5c679648088f2f2e653594e
Libo Wang <lw2768@columbia.edu>
2018-04-17 00:57:49 -0400

    modify test_parser.sh to meet shellcheck requirements

--- test_parser.sh
+++ test_parser.sh
@@ -6,28 +6,26 @@ passed=0
 
 for (( i = 0; i < pos_tests; i++ )); do
 	filename="parser_p$((i + 1)).olisp"
-	printf "\nPositive test: $filename\n"
-	./olisp.native -a "tests/$filename"
-	if [[ $? -eq 0 ]]; then
-		printf "Accepted! Test passed.\n"
+	printf "\\nPositive test: %s\\n" "$filename"
+	if ./olisp.native -a "tests/$filename"; then
+		printf "Accepted! Test passed.\\n"
 		((passed++))
 	else
-		printf "Rejected! Test failed.\n"
+		printf "Rejected! Test failed.\\n"
 	fi
 done
 
 for (( i = 0; i < neg_tests; i++ )); do
 	filename="parser_n$((i + 1)).olisp"
-	printf "\nNegative test: $filename\n"
-	./olisp.native -a "tests/$filename"
-	if [[ $? -ne 0 ]]; then
-		printf "Rejected! Test passed.\n"
-		((passed++))
+	printf "\\nNegative test: %s\\n" "$filename"
+	if ./olisp.native -a "tests/$filename"; then
+		printf "Accepted! Test failed.\\n"
 	else
-		printf "Accepted! Test failed.\n"
+		printf "Rejected! Test passed.\\n"
+		((passed++))
 	fi
 done
 
-printf "\n$passed of $((pos_tests + neg_tests)) tests passed!\n\n"
+printf "\\n%d of %d tests passed!\\n\\n" "$passed" "$((pos_tests + neg_tests))"
 
 exit $((pos_tests + neg_tests - passed))
